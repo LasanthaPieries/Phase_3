00b626e28d34c369660401e58c90cd95658afab7
lasantha_pieries <lasantha.pieries@gmail.com>
2022-12-18 11:25:41 +0530

    first commit

---
+++ src/test/java/test/endtoend/phase3/EndToEndTest.java
@@ -0,0 +1,116 @@
+/* Program Name	: Phase_3.java
+ * Date			: 11/12/2022
+ * Purpose		: Phase 3 assessment test
+ * Author		: Lasantha Pieries 
+ **********************************************************/
+package test.endtoend.phase3;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
+import io.restassured.specification.RequestSpecification;
+
+public class EndToEndTest {
+	// Declare Class level variables
+	Response response;
+	RequestSpecification request;
+	
+	String uri = "http://localhost:3000";
+	String end_point = "employees";
+	int emp_id;
+	
+	@Test
+	public void Test1() {
+		// Create Hash Map object to pass data to methods
+		Map<String, Object> MapObj = new HashMap<String, Object>();
+		
+		// Get all employees and validate for status code 200
+		Assert.assertEquals(GetAllEmployees().getStatusCode(), 200);
+		
+		/* Create an employee with name as John and salary as 8000, 
+		   then validate the response code is 201. Fetch the employee
+		   id of the newly created employee.
+		*/
+		MapObj.put("name", "John");
+		MapObj.put("salary", "8000");
+		Assert.assertEquals(CreateEmployee(MapObj).getStatusCode(), 201);
+		emp_id = response.jsonPath().getInt("id");
+		
+		// Get single employee created earlier and validate
+		// the name is John and status code is 200.
+		String ResponseBody = GetSingleEmployee().getBody().asString();
+		Assert.assertTrue(ResponseBody.contains("John"));
+		Assert.assertEquals(response.getStatusCode(), 200);
+		
+		// Update the employee created earlier to change the name 
+		// to Smith	and validate the status code is 200.
+		MapObj.put("id", emp_id);
+		MapObj.put("name", "Smith");
+		MapObj.put("salary", "8000");
+		Assert.assertEquals(UpdateEmployee(MapObj).getStatusCode(), 200);
+		
+		// Delete the employee created earlier and validate 
+		// the status code is 200.
+		Assert.assertEquals(DeleteEmployee().getStatusCode(), 200);
+
+		// Get single employee created earlier and validate
+		// the status code is 404.
+		Assert.assertEquals(GetSingleEmployee().getStatusCode(), 404);
+		
+		// Get All Employees and validate that deleted employee
+		// earlier is not present in the response.
+		Assert.assertFalse(GetAllEmployees().body().asString().contains("Smith"));
+	}
+	
+	public Response GetAllEmployees(){
+		RestAssured.baseURI = uri;
+		request = RestAssured.given();
+		response = request.get(end_point);
+		return response ;
+	}
+	
+	public Response GetSingleEmployee() {
+		RestAssured.baseURI = uri;
+		request = RestAssured.given();
+		response = request.get(end_point+"/"+emp_id);
+		return response ;
+	}
+	
+	public Response CreateEmployee(Map<String, Object> mapObj) {
+		RestAssured.baseURI = uri;
+		request = RestAssured.given();
+		response = request
+				.contentType(ContentType.JSON)
+				.accept(ContentType.JSON)
+				.body(mapObj)
+				.post(end_point+"/create");
+		return response ;
+	}
+	
+	public Response UpdateEmployee(Map<String, Object> mapObj) {
+		RestAssured.baseURI = uri;
+		request = RestAssured.given();
+		response = request
+				.contentType(ContentType.JSON)
+				.accept(ContentType.JSON)
+				.body(mapObj)
+				.put(end_point+"/"+emp_id);
+		return response ;
+	}	
+	
+	public Response DeleteEmployee() {
+		RestAssured.baseURI = uri;
+		request = RestAssured.given();
+		response = request.delete(end_point+"/"+emp_id);
+		return response;
+	}
+
+}
